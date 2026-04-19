4c478665c13c724e6ecfb84705e0e2ec86b36f7e
haeun <hekoh99@naver.com>
2026-04-19 19:40:02 -0400

    update api url

--- apps/web-angular/src/environments/environment.production.ts
+++ apps/web-angular/src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tribalance-api-dev--0000001.bluecliff-61e24533.canadacentral.azurecontainerapps.io',
+  apiUrl: 'https://tribalance-api-dev.bluecliff-61e24533.canadacentral.azurecontainerapps.io',
 };
